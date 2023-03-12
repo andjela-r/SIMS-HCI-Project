772c75147ef391c00f608552d70e9b94fba0022c
djurick <djuric.kristina01@gmail.com>
2023-03-12 15:28:04 +0100

    fixed errors

--- InitialProject/InitialProject/Model/Location.cs
+++ InitialProject/InitialProject/Model/Location.cs
@@ -2,14 +2,29 @@
 
 public class Location
 {
+    public int Id { get; set; }
 	public string City { get; set; }
-	public string Country { get; set; };
+	public string Country { get; set; }
 
-	public Location(string city, string county)
+	public Location(int id, string city, string county)
 	{
+        Id = id;
 		City = city; 
 		Country = county;
 	}
 
     public Location() { }
+
+    public string[] ToCSV()
+    {
+        string[] csvValues = { Id.ToString(), City, Country };
+        return csvValues;
+    }
+
+    public void FromCSV(string[] values)
+    {
+        Id = Convert.ToInt32(values[0]);
+        City = values[1];
+        Country = values[2];
+    }
 }
