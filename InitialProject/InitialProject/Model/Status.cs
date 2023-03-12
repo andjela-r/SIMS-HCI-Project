2fbb46e6794b3469b0b5f3ea0ead759f0ef18836
Marija Zovko <zovko.marija01@gmail.com>
2023-03-12 17:26:57 +0100

    added Tour, KeyPoint, Picture class and Status enum

---
+++ InitialProject/InitialProject/Model/Status.cs
@@ -0,0 +1,18 @@
+﻿using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+    public enum Status
+    {
+       NotStarted,
+       Ongoing,
+       Finished
+    }
+
+  
+}
