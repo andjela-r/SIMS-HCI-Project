9dcbfe4adbcb80d2ac13483f7c936f84e26a41f5
Andjela Radojevic <kixiradojevic@gmail.com>
2023-03-14 10:36:12 +0100

    Fixed mising attributes in Tour class, added TourRepository, FindBy methods. Created csv file for tour

---
+++ InitialProject/InitialProject/Repository/TourRepository.cs
@@ -0,0 +1,49 @@
+﻿using InitialProject.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repository
+{
+    public class TourRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tour.csv";
+
+        private readonly Serializer<Tour> _serializer;
+
+        private List<Tour> _tours;
+
+        public TourRepository()
+        {
+            _serializer = new Serializer<Tour>();
+            _tours = _serializer.FromCSV(FilePath);
+        }
+
+        public Tour FindByLocation(string? city, string? country)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            return _tours.FirstOrDefault(u => u.Location.City == city || u.Location.Country == country);
+        }
+
+        public Tour FindByDuration(float duration)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            return _tours.FirstOrDefault(u => u.Duration == duration);
+        }
+
+        public Tour FindByLanguage(string language)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            return _tours.FirstOrDefault(u => u.Language == language);
+        }
+
+        public Tour FindByGuestNumber(int NumberOfGuests)
+        {
+            _tours = _serializer.FromCSV(FilePath);
+            return _tours.FirstOrDefault(u => u.MaxGuests >= NumberOfGuests);
+        }
+    }
+}
