f704e379dfc9bad71e22a7454687441784be49f5
Anđela <kixiradojevic@gmail.com>
2023-04-05 10:01:25 +0200

    Added User roles and Vouchers

--- InitialProject/InitialProject/Model/User.cs
+++ InitialProject/InitialProject/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using InitialProject.Serializer;
 using System;
+using System.Linq;
 
 namespace InitialProject.Model
 {
@@ -8,18 +9,22 @@ namespace InitialProject.Model
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; }
 
         public User() { }
 
-        public User(string username, string password)
+        public User(string username, string password, Role role)
         {
             Username = username;
             Password = password;
+            Role = role;
         }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { Id.ToString(), Username, Password };
+            string[] csvValues = { Id.ToString(), Username, Password};
+            var role = Convert.ToInt32(Role);
+            csvValues = csvValues.Append(role.ToString()).ToArray();
             return csvValues;
         }
 
@@ -28,6 +33,7 @@ namespace InitialProject.Model
             Id = Convert.ToInt32(values[0]);
             Username = values[1];
             Password = values[2];
+            Role = (Role)Convert.ToInt32(values[3]);
         }
     }
 }
